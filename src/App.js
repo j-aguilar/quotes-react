515c4d64bdc4bf3cd10b0be47c97a986814fd5b4
j-aguilar <3374485+j-aguilar@users.noreply.github.com>
2020-02-17 05:09:20 -0500

    modify app to use crude-quote-api on heroku

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios'
 import './App.css';
-import data from './data'
 
-function App() {
-  const [quote, setQuote] = useState(data.get())
+function App(props) {
+  const [quote, setQuote] = useState({"text": "Loading", "author": ""})
+  const [isSending, setIsSending] = useState(false)
+  const sendRequest = useCallback(async () => {
+    if (isSending) return
+    setIsSending(true)
+    let res = {"data": {"text": "Sorry there was an error", "author": ""}}
+    try {
+      res = await axios.get('https://crude-quote-api.herokuapp.com/')
+    } catch (e) {
+      console.log(e.message)
+    } finally {
+      setIsSending(false)
+      setQuote(res.data)
+    }
+  }, [isSending])
+  React.useEffect(() => {sendRequest()}, [])
+
   return (
     <div className="App">
       <main>
         <p>{`"${quote.text}"`}</p>
         {quote.author !== "" && <p>{`--${quote.author}`}</p>}
-        <button onClick={() => setQuote(data.get())}>Random quote</button>
+        <input type="button" disabled={isSending} onClick={sendRequest} value="random quote"/>
       </main>
     </div>
   );
